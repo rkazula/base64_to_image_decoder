326925b8edda664eb6e6170d35be08b542065507
Robert Kazuła <rkazula@gmail.com>
2019-03-10 00:38:39 +0100

    renamed output filename

--- app/decoder.py
+++ app/decoder.py
@@ -7,7 +7,7 @@ data: str = "YOUR_BASE64_IMAGE_HERE"
 
 im = Image.open(BytesIO(base64.b64decode(data)))
 cropped_im = im.crop((VALUE1, VALUE2, VALUE3, VALUE4))
-cropped_im.save('nazwa.jpg')
+cropped_im.save('output_file.jpg')
 im.close()
 
 
