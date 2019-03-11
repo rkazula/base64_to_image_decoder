aa8341a0fa887174ec34d1a55bdb7d620b4815fc
Robert Kazuła <rkazula@gmail.com>
2019-03-11 12:44:52 +0100

    Added base64 verification, loading byte_array[] from the file, pytesseract OCR. Added some corrects.

--- app/decoder.py
+++ app/decoder.py
@@ -26,11 +26,16 @@ except IOError as e:
 
 # Regex for identifying Base64
 b64Marker = '(^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$)'
-b64Hit = re.search(b64Marker, text)
+b64check = re.search(b64Marker, text)
+if b64check:
+    print('Base64 found!')
+else:
+    print('File error - not base64.')
 
 
 im = Image.open(BytesIO(base64.b64decode(data)))
 cropped_im = im.crop((VALUE1, VALUE2, VALUE3, VALUE4))
 cropped_im.save('output_file.jpg')
-pytesseract.pytesseract.tesseract_cmd = r'PATH_TO_PYTESSERACT_OCR'
+pytesseract.pytesseract.tesseract_cmd = r'PATH_TO_TESSERACT_OCR'
+print('Result: ' + pytesseract.image_to_string('output_file.jpg'))
 im.close()
