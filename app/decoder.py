17e7c101b9409d753e9276c730002b48027ef7c6
Robert Kazuła <rkazula@gmail.com>
2019-03-11 14:57:14 +0100

    Added base64 verification, loading byte_array[] from the file, pytesseract OCR. Added some corrects.

--- app/decoder.py
+++ app/decoder.py
@@ -7,7 +7,7 @@ from io import BytesIO
 import pytesseract
 from PIL import Image
 
-data = 'PATH_TO_BASE64_FILE'
+data = 'PATH_TO_RESPONSE_DATA'
 
 if os.path.isfile(data):
     print('File exists: ' + str(os.path.isfile(data)))
@@ -17,8 +17,7 @@ else:
 
 try:
     response = open(data, 'r')
-    text = response.read()
-    response.close()
+    response_text = response.read()
 except IOError as e:
     print('I/O error({0}): {1}'.format(e.errno, e.strerror))
     print('File cannot be opened.')
@@ -26,16 +25,15 @@ except IOError as e:
 
 # Regex for identifying Base64
 b64Marker = '(^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$)'
-b64check = re.search(b64Marker, text)
+b64check = re.search(b64Marker, response_text)
 if b64check:
     print('Base64 found!')
 else:
     print('File error - not base64.')
 
-
-im = Image.open(BytesIO(base64.b64decode(data)))
+im = Image.open(BytesIO(base64.b64decode(response_text)))
 cropped_im = im.crop((VALUE1, VALUE2, VALUE3, VALUE4))
 cropped_im.save('output_file.jpg')
-pytesseract.pytesseract.tesseract_cmd = r'PATH_TO_TESSERACT_OCR'
+pytesseract.pytesseract.tesseract_cmd = r'PATH_TO_PYTESSERACT'
 print('Result: ' + pytesseract.image_to_string('output_file.jpg'))
 im.close()
